e2df9a87f71bd0c12576aed4b4c4892f5b0dc680
fahadhossain24 <fahadhossaim24@gmail.com>
2026-07-04 17:36:54 +0600

    chore: setup husky, lint-staged and commitlint configuration

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to Enterprise Backend Server!"', () => {
+      expect(appController.getHello()).toBe(
+        'Welcome to Enterprise Backend Server!',
+      );
     });
   });
 });
